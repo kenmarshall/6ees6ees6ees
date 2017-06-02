78e19274ff5889350031ef78f1462f5a3640573b
Kenneth Marshall <kdmarshall89@gmail.com>
2017-06-02 13:51:39 -0500

    Implement Bee as a subclass of grub
    Passed all tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
-class Bee {
-  // TODO..
+console.log(Grub);
+
+class Bee extends Grub {
+  constructor(){
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 };
